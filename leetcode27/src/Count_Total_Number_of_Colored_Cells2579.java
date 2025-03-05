070e7de61e0433ba9c4802d753864136bd521f9b
vaibhav singh <vaibhavsinghnmp@gmail.com>
2025-03-06 00:40:38 +0530

    2579. Count Total Number of Colored Cells

---
+++ leetcode27/src/Count_Total_Number_of_Colored_Cells2579.java
@@ -0,0 +1,17 @@
+public class Count_Total_Number_of_Colored_Cells2579 {
+
+//2579. Count Total Number of Colored Cells
+
+    class Solution {
+        public long coloredCells(int n) {
+            long res = n;
+            return 1 + (res - 1) * res * 2;
+        }
+    }
+}
+
+1      5       13        25       41       61
+    4      8        12        16       20
+
+
+
